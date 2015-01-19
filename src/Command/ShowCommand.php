db42732a752e8848bbf7b0fb1276c982dfa70a1a
Chris Cornutt <enygma@phpdeveloper.org>
2015-01-19 06:02:28 -0600

    adding table output to show users and groups information

--- src/Command/ShowCommand.php
+++ src/Command/ShowCommand.php
@@ -1,15 +1,95 @@
 <?php
 
-namespace Psecio\GatekeeperCli;
+namespace Psecio\GatekeeperCli\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+use Psecio\Gatekeeper\Gatekeeper;
 
 class ShowCommand extends Command
 {
+	protected function configure()
+	{
+		$this->setName('show')
+			->setDescription('Show data based on the given type')
+			->addArgument('type', null, InputArgument::REQUIRED, 'Type to show', [])
+			->setHelp('Used to show information based on the given type');
+	}
+
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-		$output->writeln('test');
+		$params = array();
+		$type = $input->getArgument('type');
+
+		$method = 'show'.$type;
+		if (!method_exists($this, $method)) {
+			throw new \InvalidArgumentException('Invalid show type: '.$type);
+		}
+
+		$this->$method($params, $output);
+	}
+
+	private function buildTable(array $columns, array $data, $output)
+	{
+		$results = array();
+		$keys = array_keys($columns);
+		foreach ($data as $record) {
+			$tmp = array();
+			foreach ($keys as $key) {
+				$tmp[] = $record[$key];
+			}
+			$results[] = $tmp;
+		}
+
+		$table = $this->getHelper('table');
+		$table->setHeaders(array_values($columns))
+			->setRows($results);
+		$table->render($output);
+	}
+
+	/**
+	 * Show the listing of users
+	 *
+	 * @param array  $params [description]
+	 * @param OutputInterface $output Output interface object
+	 */
+	public function showUsers(array $params = array(), $output)
+	{
+		$columns = array(
+			'username' => 'Username',
+			'password' => 'Password',
+			'email' => 'Email',
+			'firstName' => 'First Name',
+			'lastName' => 'Last Name',
+			'created' => 'Date Created',
+			'updated' => 'Date Updated',
+			'status' => 'Status',
+			'id' => 'ID'
+		);
+
+		$users = Gatekeeper::findUsers($params);
+		$this->buildTable($columns, $users->toArray(true), $output);
+	}
+
+	/**
+	 * Show the listing of groups
+	 *
+	 * @param array  $params [description]
+	 * @param OutputInterface $output Output interface object
+	 */
+	public function showGroups(array $params = array(), $output)
+	{
+		$columns = array(
+			'name' => 'Name',
+			'description' => 'Description',
+			'created' => 'Date Created',
+			'updated' => 'Date Updated',
+			'id' => 'ID');
+
+		$groups = Gatekeeper::findGroups($params);
+		$this->buildTable($columns, $groups->toArray(true), $output);
 	}
 }
